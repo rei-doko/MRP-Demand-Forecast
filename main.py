5f1d55ae7e204551bc290146f145f3aa4c9b4e0c
rei.doko <jr.danas23@gmail.com>
2025-09-04 21:13:28 +0800

    Updated initial setup
    Included script.js and style.css

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+#Import dependecies and libraries
+from flask import Flask, render_template, Response, jsonify, request
+from datetime import datetime
+import numpy as py
+import flaskwebgui
+import sqlite3
+import pandas as pd
+import math
+import time
+import os
 
 app = Flask(__name__)
+gui = flaskwebgui.FlaskUI(app=app, server="flask", width=1920, height=1080)
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    gui.run()
